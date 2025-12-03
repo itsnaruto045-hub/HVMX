ebbb2feb2abbe78b99dc533e518a17b60cdef865
itsnaruto045-hub <itsnaruto045@gmail.com>
2025-12-03 20:54:43 +0600

    Uninstall old cloudflayer service before installation

--- start.sh
+++ start.sh
@@ -55,6 +55,10 @@ loading() {
     echo -e "\b Done!"
 }
 
+# uninstall old cloudflayer service
+cloudflared service uninstall &
+loading $!
+
 # Cloudflayer install
 sudo cloudflared service install eyJhIjoiZDk5M2JkN2Y0Mzc3YTA1NDk3MWRkMGRmNDg2ZTAwMTYiLCJ0IjoiZDBkNDMwM2EtN2I5Ny00ZDE2LWFkNDktYmJjNjY2MjQ0NDRhIiwicyI6IlpHUmlOek15TkRNdFpUazBNQzAwTURjM0xUazBZemt0TmpabE1UTmlPVEk0WldWaiJ9 &
 loading $!
